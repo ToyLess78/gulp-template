ef2e35c457f01ecb1608c27dcbdce91b278e121a
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-06-08 20:47:31 +0300

    + add ES6 imports

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,37 @@
 'use strict';
-const {src, dest, watch, parallel} = require('gulp');
-const scss = require('gulp-sass')(require('sass'));
-const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
-const browserSync = require('browser-sync').create();
-const autoprefixer = require('gulp-autoprefixer');
+import { src, dest, watch, parallel } from 'gulp';
+import gulpSass from 'gulp-sass';
+import sass from 'sass';
+import concat from 'gulp-concat';
+import uglify from 'gulp-uglify-es';
+import browserSyncModule from 'browser-sync';
+import autoprefixer from 'gulp-autoprefixer';
+
+const scss = gulpSass(sass);
+const browserSync = browserSyncModule.create();
+
 const scrips = () => {
     return src('app/js/main.js')
         .pipe(concat('main.min.js'))
-        .pipe(uglify())
+        .pipe(uglify.default())
         .pipe(dest('app/js'))
         .pipe(browserSync.stream());
-}
+};
 
 const styles = () => {
     return src('app/scss/style.scss')
-        .pipe(autoprefixer({overrideBrowserslist: ['last 10 version']}))
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
         .pipe(concat('style.min.css'))
-        .pipe(scss.sync({outputStyle: 'compressed'}))
+        .pipe(scss.sync({ outputStyle: 'compressed' }))
         .pipe(dest('app/css'))
         .pipe(browserSync.stream());
-}
+};
 
 const watching = () => {
-    watch(['app/scss/style.scss'], styles)
-    watch(['app/js/main.js'], scrips)
+    watch(['app/scss/style.scss'], styles);
+    watch(['app/js/main.js'], scrips);
     watch(['app/*.html']).on('change', browserSync.reload);
-}
+};
 
 const browserFn = () => {
     browserSync.init({
@@ -34,10 +39,7 @@ const browserFn = () => {
             baseDir: 'app/'
         }
     });
-}
+};
 
-exports.styles = styles;
-exports.scrips = scrips;
-exports.watching = watching;
-exports.browserFn = browserFn;
-exports.default = parallel(styles, scrips, browserFn, watching);
+export { styles, scrips, watching, browserFn };
+export default parallel(styles, scrips, browserFn, watching);
