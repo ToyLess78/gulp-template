fc6e094ea623f96ebab33d77a29977a690fd0363
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-06-11 22:31:26 +0300

    + update gulp.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,26 @@
 'use strict';
-import { src, dest, watch, parallel } from 'gulp';
+import { src, dest, watch, parallel, series } from 'gulp';
 import gulpSass from 'gulp-sass';
 import sass from 'sass';
 import concat from 'gulp-concat';
 import uglify from 'gulp-uglify-es';
 import browserSyncModule from 'browser-sync';
 import autoprefixer from 'gulp-autoprefixer';
+import clean from 'gulp-clean';
+import include from 'gulp-file-include';
 
 const scss = gulpSass(sass);
 const browserSync = browserSyncModule.create();
 
-const scrips = () => {
+export const html = () => {
+    return src('app/html/*.html')
+        .pipe(include({
+            prefix: '@@'
+        }))
+        .pipe(dest('app'))
+}
+
+export const scrips = () => {
     return src('app/js/main.js')
         .pipe(concat('main.min.js'))
         .pipe(uglify.default())
@@ -18,7 +28,7 @@ const scrips = () => {
         .pipe(browserSync.stream());
 };
 
-const styles = () => {
+export const styles = () => {
     return src('app/scss/style.scss')
         .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
         .pipe(concat('style.min.css'))
@@ -27,13 +37,14 @@ const styles = () => {
         .pipe(browserSync.stream());
 };
 
-const watching = () => {
+export const watching = () => {
     watch(['app/scss/style.scss'], styles);
     watch(['app/js/main.js'], scrips);
+    watch(['app/html/**/*.html'], html);
     watch(['app/*.html']).on('change', browserSync.reload);
 };
 
-const browserFn = () => {
+export const browserFn = () => {
     browserSync.init({
         server: {
             baseDir: 'app/'
@@ -41,5 +52,20 @@ const browserFn = () => {
     });
 };
 
-export { styles, scrips, watching, browserFn };
-export default parallel(styles, scrips, browserFn, watching);
+export const building = () => {
+    return src([
+        'app/css/style.min.css',
+        'app/js/main.min.js',
+        'app/*.html'
+        ], {base: 'app'})
+        .pipe(dest('dist'))
+}
+
+export const cleanDist = () => {
+    return src('dist')
+        .pipe(clean())
+}
+
+export const build = series(cleanDist, building);
+
+export default parallel(html, styles, scrips, browserFn, watching);
